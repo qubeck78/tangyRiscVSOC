66a0eff5aed2c223dba11d9f4f997045ffbd5de8
Michal Kubecki <qubeck78@wp.pl>
2024-02-17 12:52:16 +0100

    i2s pins pllsystem sdram clock duty cycle correction

--- src/pllSystem/pllSystem_tmp.vhd
+++ src/pllSystem/pllSystem_tmp.vhd
@@ -5,7 +5,7 @@
 --Part Number: GW2AR-LV18QN88C8/I7
 --Device: GW2AR-18
 --Device Version: C
---Created Time: Thu Feb 15 22:16:34 2024
+--Created Time: Sat Feb 17 12:48:06 2024
 
 --Change the instance name and port connections to the signal names
 ----------Copy here to design--------
